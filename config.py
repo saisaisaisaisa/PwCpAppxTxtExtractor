a29ea23d11d0ed2bc758a0bd7a359d77d0ed0fb0
saisaisaisaisa <143456497+saisaisaisaisa@users.noreply.github.com>
2025-05-10 03:01:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "24473318"
-api_hash = "e7dd0576c5ac0ff8f90971d6bb04c8f5"
-bot_token = "8034069514:AAHUBpzSCq41jPwsJkDbXuEoVC_yCxzyuw0"
-auth_users = [5840594311]
+api_id = "21596316"
+api_hash = "bb72c208c6ed50893ef370301c5b5716"
+bot_token = "7527660388:AAE1_Dzw6wc9p8c-asYzr_UEZNDi_D8sYsc"
+auth_users = [7632452405]
